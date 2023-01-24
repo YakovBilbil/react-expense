26fea2d6ab8d8c117f310fa9c72f62b75a2df28c
YakovBilbil <yakov.bilbil@gmail.com>
2023-01-24 11:00:01 +0200

    finished section 4

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,15 +1,31 @@
+import { useState } from "react";
+
 import "./Expenses.css";
 
 import ExpenseItem from "../ExpenseItem/ExpenseItem.js";
 import Card from "../Card/Card.js";
+import ExpensesFilter from "../ExpensesFilter/ExpensesFilter.js";
+import React from "react";
 
 const Expenses = ({ items }) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
-    <Card className="expenses">
-      {items.map(({ id, title, amount, date }) => (
-        <ExpenseItem key={id} title={title} amount={amount} date={date} />
-      ))}
-    </Card>
+    <>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {items.map(({ id, title, amount, date }) => (
+          <ExpenseItem key={id} title={title} amount={amount} date={date} />
+        ))}
+      </Card>
+    </>
   );
 };
 
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,10 +2,18 @@ import "./NewExpense.css";
 
 import ExpenseForm from "../ExpenseForm/ExpenseForm.js";
 
-const NewExpense = () => {
+const NewExpense = ({ onAddExpense }) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    onAddExpense(expenseData);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 };
